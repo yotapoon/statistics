91b056be9e8757bf37f37a54cd9c586bd603ad2d
Yota Nonaka <47734305+yotapoon@users.noreply.github.com>
2021-07-26 00:53:15 +0900

    回帰結果をファイルとして出力するためのモジュール追加
    決定係数やAICなどをファイルに出力することができるようになった

--- my_regression.py
+++ my_regression.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd
 
 class my_linear_regression:
-    def __init__(self, X, subset = [], file = []): # 欠損値を含む可能性がある列をsubsetで指定してdrop
+    def __init__(self, X, subset = [], file_name = []): # 欠損値を含む可能性がある列をsubsetで指定してdrop
         self.X = X.copy()
         self.X.dropna(subset = subset, inplace = True)
         self.col_name_list = [] # ダミー変数にした列のリストを保存する
         self.dict_dummy_list = {} # ダミー変数の要素(dropした後のもの)を列の名前ごとに保存
-        self.file = file
+        self.file_name = file_name
         self.file_exist = False
 
     def get_dummies(self, col_name, dummy_full_list, dummy_drop): # 指定した列をダミー変数へ変更
@@ -40,18 +40,31 @@ class my_linear_regression:
         # 回帰
         self.result = sm.OLS(self.X[target], self.X[factor_list]).fit()
         # saveで使用するので保存
-        self.df_result = pd.DataFrame(index = self.result.params.index, columns = [])
-        self.df_result["coef"] = self.result.params.values
-        self.df_result["t"] = self.result.tvalues.values
+        self.df_params = pd.DataFrame(index = self.result.params.index, columns = [])
+        self.df_params["coef"] = self.result.params.values
+        self.df_params["t"] = self.result.tvalues.values
+        result_lists = ["R2", "R2_adj", "n_observations", "n_parameters", "log_likelihood", "AIC", "BIC", "mean_of_residuals"]
+        self.df_result = pd.DataFrame(index = result_lists, columns = ["value"])
+        self.df_result.at["R2", "value"] = self.result.rsquared
+        self.df_result.at["R2_adj", "value"] = self.result.rsquared_adj
+        self.df_result.at["n_observations", "value"] = self.result.nobs
+        self.df_result.at["n_parameters", "value"] = len(self.factor_list)
+        self.df_result.at["log_likelihood", "value"] = self.result.llf
+        self.df_result.at["AIC", "value"] = self.result.aic
+        self.df_result.at["BIC", "value"] = self.result.bic
+        self.df_result.at["mean_of_residuals", "value"] = self.result.mse_resid
 
         display(self.result.summary()) # 一応回帰結果を表示する
 
     def save(self, sheet_name): # 回帰係数をexcelに保存
         if self.file_exist: # 今回のユニバースでファイルが存在している場合
-            with pd.ExcelWriter(self.file, engine = "openpyxl", mode = "a") as writer: # 上書き
+            with pd.ExcelWriter(self.file_name + "_param.xlsx", engine = "openpyxl", mode = "a") as writer: # 上書き
+                self.df_params.to_excel(writer, sheet_name = sheet_name)
+            with pd.ExcelWriter(self.file_name + "_result.xlsx", engine = "openpyxl", mode = "a") as writer:
                 self.df_result.to_excel(writer, sheet_name = sheet_name)
         else: # 今回のユニバースでまだファイルが存在していない場合
-            self.df_result.to_excel(self.file, sheet_name) # ファイルの作成
+            self.df_params.to_excel(self.file_name + "_param.xlsx", sheet_name) # ファイルの作成
+            self.df_result.to_excel(self.file_name + "_result.xlsx", sheet_name)
             self.file_exist = True # フラグの更新
 
     def predict(self): # fitに使用したfactor_listでtargetを推計する
